d9090ab6adadfccabf3e8388e0d403a80a15b6bf
Callum Taylor <callum@3sidedcube.com>
2013-12-17 14:55:51 +0000

    Implemented grid adapter in example

--- example/src/main/java/net/callumtaylor/gridlistview/example/MainActivity.java
+++ example/src/main/java/net/callumtaylor/gridlistview/example/MainActivity.java
@@ -2,12 +2,39 @@ package net.callumtaylor.gridlistview.example;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import net.callumtaylor.gridlistview.GridAdapter;
 import net.callumtaylor.gridlistview.GridListView;
 
 public class MainActivity extends Activity
 {
+	private String[] items = new String[]
+	{
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"11",
+		"12",
+		"13",
+		"14",
+		"15",
+		"16",
+		"17",
+		"18",
+		"19",
+		"20"
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -16,29 +43,45 @@ public class MainActivity extends Activity
 
 		GridListView list = (GridListView)findViewById(R.id.list_view);
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.column, android.R.id.text1, new String[]
+		GridAdapter adapter = new GridAdapter()
 		{
-			"1",
-			"2",
-			"3",
-			"4",
-			"5",
-			"6",
-			"7",
-			"8",
-			"9",
-			"10",
-			"11",
-			"12",
-			"13",
-			"14",
-			"15",
-			"16",
-			"17",
-			"18",
-			"19",
-			"20"
-		});
+			@Override public int getCount()
+			{
+				return items.length;
+			}
+
+			@Override public String getItem(int position)
+			{
+				return items[position];
+			}
+
+			@Override public long getItemId(int position)
+			{
+				return 0;
+			}
+
+			@Override public int getColumnCount(int rowIndex)
+			{
+				if (rowIndex % 2 == 0)
+				{
+					return 1;
+				}
+
+				return super.getColumnCount(rowIndex);
+			}
+
+			@Override public View getView(int position, View convertView, ViewGroup parent)
+			{
+				if (convertView == null)
+				{
+					convertView = getLayoutInflater().inflate(R.layout.column, parent, false);
+				}
+
+				((TextView)convertView.findViewById(android.R.id.text1)).setText(getItem(position));
+
+				return convertView;
+			}
+		};
 
 		list.setAdapter(adapter);
 	}
